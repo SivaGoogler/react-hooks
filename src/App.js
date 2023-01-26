2f06fd505eaba2be4c9c860232c9fd5a77b46a72
SivaGoogler <62846065+SivaGoogler@users.noreply.github.com>
2023-01-26 17:25:05 +0530

    counter added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './style.css';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      theme: 'blue',
+    };
+  }
+
+  incrementCounter = () => {
+    this.setState((prevState) => {
+      return { count: prevState.count + 1 };
+    });
+  };
+
+  render() {
+    const { count, theme } = this.state;
+    return (
+      <>
+        <button onClick={this.incrementCounter}>Click</button>
+        <span>{` count: ${count} theme: ${theme} `}</span>
+        <button onClick={this.incrementCounter}>Click</button>
+      </>
+    );
+  }
 }
